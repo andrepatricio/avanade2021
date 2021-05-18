7f4a8cfcd57617062fc886e6cf98f7264df07d26
andrepatricio <andrepatricio2@gmail.com>
2021-05-17 22:19:43 -0300

    design patterns

---
+++ strategy/empresaStrategy.ts
@@ -0,0 +1,8 @@
+export default interface EmpresaStrategy {
+    tarifaKM: number
+    tarifaHR: number
+    
+    calculaFrete(distancia: number): string
+    calculaTempo(distancia: number): string
+}
+
